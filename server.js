18811c6178576f1ccf09539952578d3f6b310388
Kevin Shank <Kevin_Shank@yahoo.com>
2021-10-07 15:58:09 -0400

    Add mongoose connection for Heroku deployment

--- server.js
+++ server.js
@@ -16,10 +16,21 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
+/*Before deploying to Heroku
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
-});
+});*/
 
 // routes
 app.use(require("./routes/api.js"));
